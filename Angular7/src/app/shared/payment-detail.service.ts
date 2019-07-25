e10d3a0313a7a98b66c429e9fe8217556de07d2c
mbambosiyabonga@gmail.com <mbambosiyabonga@gmail.com>
2019-07-25 15:34:57 +0200

    added asp.net api 2

---
+++ Angular7/src/app/shared/payment-detail.service.ts
@@ -0,0 +1,30 @@
+import { PaymentDetail } from './payment-detail.model';
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentDetailService {
+  formData: PaymentDetail;
+  readonly rootURL = 'http://localhost:50214/api';
+  list : PaymentDetail[];
+
+  constructor(private http: HttpClient) { }
+
+  postPaymentDetail() {
+    return this.http.post(this.rootURL + '/PaymentDetails', this.formData);
+  }
+  putPaymentDetail() {
+    return this.http.put(this.rootURL + '/PaymentDetails/'+ this.formData.PMId, this.formData);
+  }
+  deletePaymentDetail(id) {
+    return this.http.delete(this.rootURL + '/PaymentDetails/'+ id);
+  }
+
+  refreshList(){
+    this.http.get(this.rootURL + '/PaymentDetails')
+    .toPromise()
+    .then(res => this.list = res as PaymentDetail[]);
+  }
+}
